c786431aa9f87bef9ebe4535f1f33ff64174fb55
Cherry <cherry@sparklet.org>
2023-11-20 10:53:56 -0500

    less args (automatically does version + we don't really need the stdin flag)

--- src/main.rs
+++ src/main.rs
@@ -1,40 +1,30 @@
-/*
-* def parse_args():
-    parser = ArgumentParser()
-    parser.add_argument("-o", "--output", help="output file name")
-
-    g = parser.add_mutually_exclusive_group(required=True)
-    g.add_argument("-v", "--version", help="version info", action="store_true")
-    g.add_argument("-s", "--stdin", help="read from stdin", action="store_true")
-    g.add_argument("file", help="definition file", nargs="?")
-
-    # show help if no args given
-    # "borrowed, not stolen"
-    # - Ferris the Crab
-    return parser.parse_args(args=None if sys.argv[1:] else ["--help"])
-
-* */
+//!
+//! literally rewriting it in rust because i got tired
+//! of trying to make modules work properly instead of
+//! being able to write actual code
+//!
+//!  - &Cherry, 11/20/2023
+//!
 
 use clap::Parser;
 
-/// Simple program to greet a person
 #[derive(Parser, Debug)]
-#[command(author, version, about, long_about = None)]
-struct Args {
-    /// Name of the person to greet
-    #[arg(short, long)]
-    name: String,
+#[command(author, version, about)]
 
-    /// Number of times to greet
-    #[arg(short, long, default_value_t = 1)]
-    count: u8,
+/// P/E/T/S Dialogue Compiler
+struct Args {
+    #[arg(short, long)]
+    output: String,
+
+    // if `None`, read stdin
+    file: Option<String>,
 }
 
 fn main() {
     // parse args
-    let args = Args::parse();
+    let _args = Args::parse();
 
-    for _ in 0..args.count {
-        println!("Hello {}!", args.name)
-    }
+    // for _ in 0..args.count {
+    //     println!("Hello {}!", args.name)
+    // }
 }
