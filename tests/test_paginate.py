ab27d0fb91925dcbb2c91d124dce5b4fd92be7bc
Cherry <cherry@sparklet.org>
2023-11-20 09:19:18 -0500

    test case for many pages

--- tests/test_paginate.py
+++ tests/test_paginate.py
@@ -16,6 +16,41 @@ When the words are sus
 """
 )
 
+MANY_PAGES = StringIO(
+    """---
+%Interaction
+
+---
+NAME Deez
+VOX Deez
+
+When the words are sus
+
+---
+NAME Gamer
+
+Words go brrr
+
+---
+
+When the imposter is sus
+
+---
+###
+
+// Another Page
+Echo hello world
+
+###
+---
+NAME Siva
+VOX Siva
+
+---
+
+"""
+)
+
 
 class PaginateTest(unittest.TestCase):
     def test_one_page(self):
@@ -32,6 +67,51 @@ class PaginateTest(unittest.TestCase):
                     "VOX Deez\n",
                     "\n",
                     "When the words are sus\n",
+                ],
+            ],
+        )
+
+    def test_many_pages(self):
+        res = paginate(MANY_PAGES)
+        self.assertEqual(
+            res,
+            [
+                [
+                    "%Interaction\n",
+                    "\n",
+                ],
+                [
+                    "NAME Deez\n",
+                    "VOX Deez\n",
+                    "\n",
+                ],
+                [
+                    "When the words are sus\n",
+                    "\n",
+                ],
+                [
+                    "NAME Gamer\n",
+                    "\n",
+                    "Words go brrr\n",
+                    "\n",
+                ],
+                [
+                    "\n",
+                    "When the imposter is sus\n",
+                    "\n",
+                ],
+                [
+                    "###" "\n",
+                    "\n",
+                    "// Another Page\n",
+                    "Echo hello world\n",
+                    "\n",
+                    "###\n",
+                ],
+                [
+                    "NAME Siva\n",
+                    "VOX Siva\n",
+                    "\n",
                 ],
             ],
         )
