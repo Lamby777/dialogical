056f39e156d98f098044494ae912ccc7a468f203
Cherry <cherry@sparklet.org>
2023-11-20 11:26:12 -0500

    rm reference comment

--- src/parser.rs
+++ src/parser.rs
@@ -1,33 +1,6 @@
-/*
-def parse_dg(data):
-    pages = paginate(data)
-    pages = [Page(page) for page in pages]
-
-    for page in pages:
-        print(page)
-
-
-def paginate(data) -> list[list[str]]:
-    pages = []
-    page = []
-
-    def push_page(page):
-        # don't push page if empty
-        if list(filter(lambda x: x != "\n", page)):
-            pages.append(page)
-
-    for line in data.readlines():
-        if line == "---\n":
-            push_page(page)
-            page = []
-        else:
-            page.append(line)
-
-    # push the last page if not empty
-    push_page(page)
-
-    return pages
-*/
+//!
+//! Stuff for parsing dg files
+//!
 
 /// possible states the parser can be in
 enum ParseState {
