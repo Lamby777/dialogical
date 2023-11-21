bb3815b480d377d4920e06cf4a501f9b60846cb7
Cherry <cherry@sparklet.org>
2023-11-21 13:09:26 -0500

    comptime mod.rs boilerplate

--- src/comptime/mod.rs
+++ src/comptime/mod.rs
@@ -1,3 +1,25 @@
-fn exec_comptime(_code: &str) -> Result<(), ()> {
-    Ok(())
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum ScriptError {
+    #[error("Panic for whatever dumb reason")]
+    TestPanic,
+}
+
+type Result<T> = std::result::Result<T, ScriptError>;
+
+fn exec_comptime(_code: &str) -> Result<String> {
+    todo!()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn hello_world() {
+        let code = r#"print("Hello, world!")"#;
+        let res = exec_comptime(code);
+        assert_eq!(res.unwrap(), "Hello, world!");
+    }
 }
