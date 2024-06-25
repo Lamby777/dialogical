f2dc431f2579e200ad46cb1fd32c28c343861bf8
Cherry <cherry@sparklet.org>
2024-06-25 15:57:13 -0400

    rm unused error type

--- src/pages.rs
+++ src/pages.rs
@@ -74,9 +74,6 @@ pub enum ParseError {
     #[error("Attempt to push a page after an ending in interaction")]
     PageAfterEnding,
 
-    #[error("Function label arguments could not be parsed at: {0}.")]
-    BadFnArgs(String),
-
     #[error("Failed while running comptime script")]
     Panic(ScriptError),
 }
